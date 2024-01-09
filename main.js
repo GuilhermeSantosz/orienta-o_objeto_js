f8f7cc3e28498b1305f78143b6afbadf5d97136a
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-01-09 10:09:35 -0300

    tipos e instâncias

--- main.js
+++ main.js
@@ -11,3 +11,24 @@ function Carro(modelo, fabricante, anoModelo, anoFabricacao) {
 const carroJoao = new Carro("Fiesta", "Ford", 2020, 2019)
 
 console.log(carroJoao)
+
+const nome = "guilherme"
+const idade = 19
+const maiorDeIdade = true
+const conhecimentos = ["html", "css", "javascript"]
+
+const pessoa = {
+    nome: nome,
+    idade: idade,
+    maiorDeIdade: maiorDeIdade,
+    conhecimentos: conhecimentos,
+}
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof maiorDeIdade)
+console.log(typeof conhecimentos)
+console.log(typeof pessoa)
+
+console.log(carroJoao instanceof Carro)
+console.log(conhecimentos instanceof Carro)
