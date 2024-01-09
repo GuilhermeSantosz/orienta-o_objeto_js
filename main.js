0209f9777c4df7cdc11843c2f008b4cb118aa8b8
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-01-09 10:26:03 -0300

    acessando atributos

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function Carro(modelo, fabricante, anoModelo, anoFabricacao) {
 
 const carroJoao = new Carro("Fiesta", "Ford", 2020, 2019)
 
-console.log(carroJoao)
+// console.log(carroJoao)
 
 const nome = "guilherme"
 const idade = 19
@@ -24,11 +24,40 @@ const pessoa = {
     conhecimentos: conhecimentos,
 }
 
-console.log(typeof nome)
-console.log(typeof idade)
-console.log(typeof maiorDeIdade)
-console.log(typeof conhecimentos)
-console.log(typeof pessoa)
 
-console.log(carroJoao instanceof Carro)
-console.log(conhecimentos instanceof Carro)
+function exibeAtributo(nomeAtributo) {
+    console.log(pessoa[nomeAtributo])
+}
+
+exibeAtributo('idade')
+
+pessoa['sobrenome'] = undefined
+
+Object.freeze(pessoa);
+
+pessoa.nome = 'gabriel'
+
+if (pessoa["sobrenome"]) {
+    console.log("a pessoa tem um sobrenome")
+}
+
+if ('sobrenome' in pessoa) {
+    console.log('tem sobrenome')
+}
+
+console.log(pessoa.nome)
+console.log(pessoa["idade"])
+
+console.log(Object.keys(pessoa).length)
+console.log(Object.values(pessoa))
+
+
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof maiorDeIdade)
+// console.log(typeof conhecimentos)
+// console.log(typeof pessoa)
+
+// console.log(carroJoao instanceof Carro)
+// console.log(conhecimentos instanceof Carro)
