32bef54a52bf7042482c009dcb6dee2b40d97f70
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-01-09 20:20:56 -0300

    encapsulamento

--- funcionario.js
+++ funcionario.js
@@ -10,7 +10,22 @@ function Pessoa(nome) {
 
 function Funcionario(nome, cargo, salario) {
     this.cargo = cargo
-    this.salario = salario
+    // this.salario = salario
+    let _salario = salario
+
+    this.getSalario = function() {
+        return `O sálario de ${this.nome} é ${_salario}`
+    }
+
+    this.setSalario = function(valor) {
+        if (typeof valor === 'number'){
+            _salario = valor
+        }
+    }
+    
+    this.dizCargo = function() {
+        console.log(this.cargo)
+    }
 
     Pessoa.call(this, nome)
 }
@@ -20,5 +35,8 @@ const funcionario1 = new Funcionario("Maria", "dev front-end", 5000)
 const pessoa = new Pessoa("guilherme")
 
 funcionario1.dizOi()
-pessoa.dizCargo()
+funcionario1.dizCargo()
 
+funcionario1.setSalario('mil')
+
+console.log(funcionario1.getSalario())
